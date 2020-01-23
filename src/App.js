cad733b4c014800f48176a0479d156f6aa1b3bcb
unknown <william.z.wilson95@gmail.com>
2020-01-23 15:55:12 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+import Title from "./components/title"
+import Image from "./components/image"
+import Description from "./components/description"
+import Date from "./components/date"
+import Copyright from "./components/copyright"
 
 function App() {
+const [data, setData] = useState ([])
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=bqSg8cRP3XnjA62GFdp9aRliSIROLvR18b2wZCD0")
+    .then(response => {
+      console.log(response)
+      setData(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+    <Title info={data}/>\
+    <Image img={data}/>
+    <Description data={data}/>
+    <Date date={data}/>
+    <Copyright Copyright={data}/>
     </div>
   );
 }
---
+++ src/components/description.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react"
+
+
+function Description (props) {
+    return(
+        <p>{props.data.explanation}</p>
+    )
+
+}
+
+export default Description
